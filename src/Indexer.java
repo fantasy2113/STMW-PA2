8c09010075b52384600660dee0512f286bbd7a4d
jos <julian.osmer@outlook.de>
2021-01-13 11:06:14 +0100

    add

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,102 +1,103 @@
-import java.lang.*;
-import java.util.*;
-import java.sql.*;
-
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.analysis.core.SimpleAnalyzer;
-import org.apache.lucene.document.*;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Indexer {
 
-    public Indexer() {
+  public static IndexWriter indexWriter;
+
+  public Indexer() {
+  }
+
+  public static void main(String[] args) throws Exception {
+    String usage = "java Indexer";
+    Collection<Item> items = getItems();
+    rebuildIndexes("indexes", items);
+  }
+
+
+  public static void insertDoc(IndexWriter indexWriter, Item item) {
+    Document doc = new Document();
+    doc.add(new LongField("id", item.getItemId(), Field.Store.YES));
+    doc.add(new TextField("line", item.getLine().trim(), Field.Store.YES));
+    doc.add(new TextField("name", item.getName().trim(), Field.Store.YES));
+    doc.add(new FloatField("price", item.getPrice(), Field.Store.YES));
+    doc.add(new TextField("has_coordinates", item.isHasCategory() ? "true" : "false", Field.Store.YES));
+    try {
+      indexWriter.addDocument(doc);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public static IndexWriter indexWriter;
-
-    public static void main(String args[]) throws Exception {
-        String usage = "java Indexer";
-        Collection<Item> items = getItems();
-        rebuildIndexes("indexes", items);
+  public static void rebuildIndexes(String indexPath, Collection<Item> items) {
+    try {
+      Path path = Paths.get(indexPath);
+      System.out.println("Indexing to directory '" + indexPath + "'...\n");
+      Directory directory = FSDirectory.open(path);
+      IndexWriterConfig config = new IndexWriterConfig(new SimpleAnalyzer());
+      IndexWriter indexWriter = new IndexWriter(directory, config);
+      indexWriter.deleteAll();
+      for (Item item : items) {
+        insertDoc(indexWriter, item);
+      }
+      indexWriter.close();
+      directory.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-
-    public static void insertDoc(IndexWriter indexWriter, Item item) {
-        Document doc = new Document();
-        doc.add(new LongField("id", item.getItemId(), Field.Store.YES));
-        doc.add(new TextField("line", item.getLine().trim(), Field.Store.YES));
-        doc.add(new TextField("name", item.getName().trim(), Field.Store.YES));
-        doc.add(new FloatField("price", item.getPrice(), Field.Store.YES));
-        doc.add(new TextField("has_coordinates", item.isHasCategory() ? "true" : "false", Field.Store.YES));
-        try {
-            indexWriter.addDocument(doc);
-        } catch (Exception e) {
-            e.printStackTrace();
+  public static Collection<Item> getItems() throws SQLException {
+    System.out.println("Fetch items...");
+    Map<Long, Item> itemMap = new HashMap<>();
+    Connection conn = null;
+    Statement stmt = null;
+    try {
+      conn = DbManager.getConnection(true);
+      stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT item.*,  item_coordinates.latitude, buy_price.buy_price, has_category.category_name FROM item LEFT JOIN item_coordinates ON item.item_id = item_coordinates.item_id LEFT JOIN has_category ON item.item_id = has_category.item_id LEFT JOIN buy_price ON item.item_id = buy_price.item_id ORDER BY item.item_id ASC;");
+      while (rs.next()) {
+        final long item_id = rs.getLong("item_id");
+        if (!itemMap.containsKey(item_id)) {
+          Item item = new Item();
+          item.setItemId(item_id);
+          item.setName(rs.getString("item_name"));
+          item.setLine(rs.getString("item_name"));
+          item.setLine(rs.getString("description"));
+          item.setLine(rs.getString("category_name"));
+          if (rs.getObject("latitude") != null) {
+            item.setHasCategory(true);
+          }
+          if (rs.getObject("buy_price") != null) {
+            item.setPrice(rs.getFloat("buy_price"));
+          }
+          itemMap.put(item_id, item);
+        } else {
+          Item item = itemMap.get(item_id);
+          item.setLine(rs.getString("category_name"));
         }
+      }
+    } catch (
+      SQLException ex) {
+      ex.printStackTrace();
+    } finally {
+      if (stmt != null) stmt.close();
+      if (conn != null) conn.close();
     }
-
-    public static void rebuildIndexes(String indexPath, Collection<Item> items) {
-        try {
-            Path path = Paths.get(indexPath);
-            System.out.println("Indexing to directory '" + indexPath + "'...\n");
-            Directory directory = FSDirectory.open(path);
-            IndexWriterConfig config = new IndexWriterConfig(new SimpleAnalyzer());
-            // IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
-            // IndexWriterConfig config = new IndexWriterConfig(new EnglishAnalyzer());
-            IndexWriter indexWriter = new IndexWriter(directory, config);
-            indexWriter.deleteAll();
-            for (Item item : items) {
-                insertDoc(indexWriter, item);
-            }
-            indexWriter.close();
-            directory.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Collection<Item> getItems() throws SQLException {
-        System.out.println("Fetch items...");
-        Map<Long, Item> itemMap = new HashMap<>();
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            conn = DbManager.getConnection(true);
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT item.*,  item_coordinates.latitude, buy_price.buy_price, has_category.category_name FROM item LEFT JOIN item_coordinates ON item.item_id = item_coordinates.item_id LEFT JOIN has_category ON item.item_id = has_category.item_id LEFT JOIN buy_price ON item.item_id = buy_price.item_id ORDER BY item.item_id ASC;");
-            while (rs.next()) {
-                final long item_id = rs.getLong("item_id");
-                if (!itemMap.containsKey(item_id)) {
-                    Item item = new Item();
-                    item.setItemId(item_id);
-                    item.setName(rs.getString("item_name"));
-                    item.setLine(rs.getString("item_name"));
-                    item.setLine(rs.getString("description"));
-                    item.setLine(rs.getString("category_name"));
-                    if (rs.getObject("latitude") != null) {
-                        item.setHasCategory(true);
-                    }
-                    if (rs.getObject("buy_price") != null) {
-                        item.setPrice(rs.getFloat("buy_price"));
-                    }
-                    itemMap.put(item_id, item);
-                } else {
-                    Item item = itemMap.get(item_id);
-                    item.setLine(rs.getString("category_name"));
-                }
-            }
-        } catch (
-                SQLException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (stmt != null) stmt.close();
-            if (conn != null) conn.close();
-        }
-        return itemMap.values();
-    }
+    return itemMap.values();
+  }
 }
--- src/Searcher.java
+++ src/Searcher.java
@@ -1,81 +1,82 @@
-import java.lang.*;
-import java.nio.file.Paths;
-
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.analysis.core.SimpleAnalyzer;
-import org.apache.lucene.document.*;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Searcher {
 
-    private static String width = null;
-    private static String lat = null;
-    private static String lon = null;
-    private static boolean isGeo = false;
+  private static String width = null;
+  private static String lat = null;
+  private static String lon = null;
+  private static boolean isGeo = false;
 
-    public Searcher() {
-    }
+  public Searcher() {
+  }
 
-    public static void main(String[] args) throws Exception {
-        String usage = "java Searcher";
-        setInput(args);
-        search(args[0], "indexes");
-    }
+  public static void main(String[] args) throws Exception {
+    String usage = "java Searcher";
+    setInput(args);
+    search(args[0], "indexes");
+  }
 
-    private static TopDocs search(String searchText, String p) {
-        System.out.println("Running search(" + searchText + ")");
-        try {
-            Path path = Paths.get(p);
-            Directory directory = FSDirectory.open(path);
-            IndexReader indexReader = DirectoryReader.open(directory);
-            IndexSearcher indexSearcher = new IndexSearcher(indexReader);
-            QueryParser queryParser = new QueryParser("line", new SimpleAnalyzer());
-            Query query = queryParser.parse(getQuery(searchText));
-            TopDocs topDocs = indexSearcher.search(query, 10000);
-            System.out.println("Number of Hits: " + topDocs.totalHits);
-            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-                Document doc = indexSearcher.doc(scoreDoc.doc);
-                System.out.println("id: " + doc.get("id") + ", " + doc.get("name") + ", score: " + scoreDoc.score + ", price: " + doc.get("price"));
-            }
-            return topDocs;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+  private static TopDocs search(String searchText, String p) {
+    System.out.println("Running search(" + searchText + ")");
+    try {
+      Path path = Paths.get(p);
+      Directory directory = FSDirectory.open(path);
+      IndexReader indexReader = DirectoryReader.open(directory);
+      IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+      QueryParser queryParser = new QueryParser("line", new SimpleAnalyzer());
+      Query query = queryParser.parse(getQuery(searchText));
+      TopDocs topDocs = indexSearcher.search(query, 10000);
+      System.out.println("Number of Hits: " + topDocs.totalHits);
+      for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+        Document doc = indexSearcher.doc(scoreDoc.doc);
+        System.out.println("id: " + doc.get("id") + ", " + doc.get("name") + ", score: " + scoreDoc.score + ", price: " + doc.get("price"));
+      }
+      return topDocs;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
     }
+  }
 
-    private static void setInput(String... args) throws Exception {
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-x")) {
-                lon = args[i + 1];
-            }
-            if (args[i].equals("-y")) {
-                lat = args[i + 1];
-            }
-            if (args[i].equals("-w")) {
-                width = args[i + 1];
-            }
-        }
-        if (lon != null && lat != null && width != null) {
-            isGeo = true;
-        }
+  private static void setInput(String... args) throws Exception {
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals("-x")) {
+        lon = args[i + 1];
+      }
+      if (args[i].equals("-y")) {
+        lat = args[i + 1];
+      }
+      if (args[i].equals("-w")) {
+        width = args[i + 1];
+      }
     }
+    if (lon != null && lat != null && width != null) {
+      isGeo = true;
+    }
+  }
 
-    private static String getQuery(String searchText) {
-        String[] terms = searchText.split(" ");
-        StringBuilder sb = new StringBuilder("line:" + terms[0]);
-        for (int i = 1; i < terms.length; i++) {
-            sb.append(" OR line:").append(terms[i]);
-        }
-        if (isGeo) {
-            sb.append(" AND has_coordinates:true");
-        }
-        return sb.toString();
+  private static String getQuery(String searchText) {
+    String[] terms = searchText.split(" ");
+    StringBuilder sb = new StringBuilder("line:" + terms[0]);
+    for (int i = 1; i < terms.length; i++) {
+      sb.append(" OR line:").append(terms[i]);
     }
+    if (isGeo) {
+      sb.append(" AND has_coordinates:true");
+    }
+    return sb.toString();
+  }
 }
