e080fb58785dd32b946c7fd80d2c13b5e699086c
jos <julian.osmer@outlook.de>
2021-01-13 20:19:39 +0100

    add

--- createSpatialIndex.sql
+++ createSpatialIndex.sql
@@ -17,12 +17,22 @@ INSERT INTO spatial_index (item_id, coord)
 SELECT item_coordinates.item_id, Point(item_coordinates.longitude, item_coordinates.latitude)
 FROM item_coordinates;
 
-CREATE TABLE has_category_idx
+CREATE TABLE IF NOT EXISTS has_category_idx
 (
-    item_id       INT(11) NOT NULL,
-    category_name VARCHAR(128) NOT NULL,
-    PRIMARY KEY (item_id, category_name)
-);
+    item_id INT
+(
+    11
+) NOT NULL,
+    category_name VARCHAR
+(
+    128
+) NOT NULL,
+    PRIMARY KEY
+(
+    item_id,
+    category_name
+)
+    );
 
 INSERT INTO has_category_idx (item_id, category_name)
 SELECT has_category.item_id, has_category.category_name
